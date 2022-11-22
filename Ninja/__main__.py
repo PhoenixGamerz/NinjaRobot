a9aa26e06ab54ca152f189c584460397ff121726
Phoenix Gamerz <114356392+PhoenixGamerz@users.noreply.github.com>
2022-11-22 23:44:16 +0530

    Update __main__.py

--- Ninja/__main__.py
+++ Ninja/__main__.py
@@ -1,1 +1,32 @@
-#rukja kar raha hu
+import os
+import logging
+import pyrogram
+from os import getenv
+from pyrogram import Client
+import config
+from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.WARNING,
+)
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+OWNER_ID = int(getenv("OWNER_ID", "O"))
+
+linda = "🎉 sᴜᴄᴄᴇsғᴜʟʟʏ ᴅᴇᴘʟᴏʏᴇᴅ ʏᴏᴜʀ ʙᴏᴛ!!"
+tinda = "🤣 ᴀʀᴇᴇ ʙʜᴀɪ ᴀʙ ʙᴀɴ ɢᴀʏᴀ ʜᴀɪ ᴄʜᴀɴɴᴇʟ ᴊᴏɪɴ ᴋᴀʀʟᴇ."
+
+if __name__ == "__main__":
+    print()
+    print()
+    app = pyrogram.Client(
+         "Ninja",
+         config.API_ID,
+         config.API_HASH,
+         bot_token=BOT_TOKEN,
+         plugins=dict(root="plugins")
+)
+         app.run()
