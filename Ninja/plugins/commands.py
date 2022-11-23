96b7972cc021dba4a9cf788227a598c0d4eb2a6e
Phoenix Gamerz <114356392+PhoenixGamerz@users.noreply.github.com>
2022-11-23 10:20:18 +0530

    Update commands.py

--- Ninja/plugins/commands.py
+++ Ninja/plugins/commands.py
@@ -1,4 +1,8 @@
 from typing import Union, List
 from pyrogram import filters
 
-other_filters = filters.group & ~ filters.edited & ~filters.via_bot & ~ filter.forwaded
+other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filter.forwarded
+other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+
+def commandpro(commands: Union[str, List[str]]):
+    return filters.command(commands, "")
