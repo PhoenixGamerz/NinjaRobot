89bf78c9e179db53f4ebbc2c3f6ae13c6e97cfb5
Phoenix Gamerz <114356392+PhoenixGamerz@users.noreply.github.com>
2022-12-01 22:50:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from os import getenv
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "14009924"))
+API_HASH = getenv("API_HASH", "0d7190eebd87743ea0360568efc35d64")
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID"))
